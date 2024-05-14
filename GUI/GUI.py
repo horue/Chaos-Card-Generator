942e1254c9a26dbaddb0a053199064f68a828b10
Jorge <jorgejapao00@gmail.com>
2024-05-13 23:24:59 -0300

    Entrys for name and effect added

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,6 +3,14 @@ import customtkinter as ct
 def initial(root):
     l1 = ct.CTkLabel(root, text='Chaos Card Generator - Main Screen')
     l1.pack()
+
+    e1 = ct.CTkEntry(root, placeholder_text='Card Name', width=200, border_color='Gray')
+    e1.pack(padx=10, pady=20)
+
+    e2 = ct.CTkTextbox(root, height=200, width=200, border_color='Gray', border_width=2)
+    e2.pack(padx=100)
+
+
     print(1)
 
 
