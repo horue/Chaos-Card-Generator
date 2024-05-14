f99b4302582cf39a0fd03134535187bd549bed0d
Jorge <jorgejapao00@gmail.com>
2024-05-14 08:52:06 -0300

    General frame 1 configuration changed and frame2 added

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,11 +1,18 @@
 import customtkinter as ct
 
 def initial(root):
-    f1=ct.CTkScrollableFrame(root, width=400)
-    f1.grid(column=0, row=1, padx=10)
+    f0=ct.CTkScrollableFrame(root, width=400, height=300)
+    f0.grid(column=0, row=1, padx=30)
+
+    f1=ct.CTkScrollableFrame(root, width=400, height=300)
+    f1.grid(column=0, row=1, padx=30)
+    
+    f2=ct.CTkScrollableFrame(root, width=400, height=300)
+    f2.grid(column=0, row=2, padx=30, pady=30)
+
 
     l1 = ct.CTkLabel(root, text='Chaos Card Generator - Main Screen')
-    l1.grid(column=1, row=0)
+    l1.grid(column=3, row=0)
 
     e1 = ct.CTkEntry(f1, placeholder_text='Card Name', width=200, border_color='Gray')
     e1.pack(padx=10, pady=20)
