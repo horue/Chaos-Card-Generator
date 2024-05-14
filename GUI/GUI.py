a5654d1195d03d3df068122ef226567636b14fee
Jorge <jorgejapao00@gmail.com>
2024-05-13 21:14:03 -0300

    Main screen created

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,1 +1,20 @@
-import customtkinter
+import customtkinter as ct
+
+def initial(root):
+    print(1)
+
+
+def main_screen():
+    root = ct.CTk()
+    root.geometry("400x500")
+    root.title("Chaos Card Generator")
+
+    
+    initial(root)
+    
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+
+main_screen()
