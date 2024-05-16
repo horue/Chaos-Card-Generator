f97aba00401df2e65ff9a8ca94efd1a0ac49fc49
Jorge <jorgejapao00@gmail.com>
2024-05-16 08:23:02 -0300

    Second scroll frame removed, first one expanded

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,33 +1,41 @@
 import customtkinter as ct
 from CTkMenuBar import *
-from toolbar import *
+from  CustomTkinterMessagebox  import  *
+from customtkinter import filedialog    
+
+
+
+def get_command(value):
+    print("segmented button clicked:", value)
+    if value == 'Save':
+        CTkMessagebox.messagebox(title='Saving test', text='Save')
+    if value == 'Open':
+        filedialog.askopenfilename()
+    o1.set("")
 
 def bar1(fm):
-    f0=ct.CTkFrame(fm, width=99, height=20)
-    f0.grid(column=0, row=0, padx=0, pady=(0,30), sticky='ew', columnspan=5)
+    #f0=ct.CTkFrame(fm, width=99, height=20)
+    #f0.grid(row=0, padx=0, pady=(0,30), sticky='ew', columnspan=True)
 
     global o1
-    o1 = ct.CTkSegmentedButton(f0, values=["Save", "Open", "Export", ""],corner_radius=50,fg_color='#dbdbdb', selected_color='#dbdbdb',unselected_color='#dbdbdb', text_color='black',selected_hover_color='#c0c0c0',command=get_command)
+    o1 = ct.CTkSegmentedButton(fm, values=["Save", "Open", "Export", ""],corner_radius=50,fg_color='#dbdbdb', selected_color='#dbdbdb',unselected_color='#dbdbdb', text_color='black',selected_hover_color='#c0c0c0',command=get_command)
     o1.set("")
     selected_value = o1.get()
     o1.delete(selected_value)
     o1.grid(row=2, column=0, pady=10, padx=10)
 
 def initial(root):
+    fh=ct.CTkFrame(root)
+    fh.pack(fill='x', anchor='n')
 
-    fm=ct.CTkFrame(root)
-    fm.pack(fill='both', expand=True, side='top')
-
-    bar1(fm)
-
-    f1=ct.CTkScrollableFrame(fm, width=400, height=300)
-    f1.grid(column=0, row=1, padx=30)
+    fm=ct.CTkFrame(root, height=20, fg_color='#ffffff')
+    fm.pack(fill='both', expand=True, anchor='s')
     
-    f2=ct.CTkScrollableFrame(fm, width=400, height=300)
-    f2.grid(column=0, row=2, padx=30, pady=30)
+    bar1(fh)
 
-    f3=ct.CTkFrame(fm, width=500, height=700)
-    f3.grid(column=5, row=1)
+    f1=ct.CTkScrollableFrame(fm, width=400, height=650)
+    f1.grid(column=0, row=1, padx=30, pady=(30,0))
+    
     l1a = ct.CTkLabel(f1, text="Card Name")
     l1a.pack()
 
