8176711b47534eb5a33c1d51097a8729b7013415
Jorge <jorgejapao00@gmail.com>
2024-05-18 09:26:15 -0300

    Function for change card name created with input

--- cardgenerator.py
+++ cardgenerator.py
@@ -3,16 +3,19 @@ import PIL
 
 ps = Dispatch(r"Photoshop.Application")
 file_path = r'C:\Users\jorge\Projetos\Chaos-Card-Generator\PS_templates\ps_template.psd'
-
-
 docRef = ps.Open(file_path)
-group1_name = 'Criatura'
-
-group = docRef.LayerSets[group1_name]
 
 
-card_name = group.ArtLayers['namea']
-card_name.TextItem.Contents = 'Artilharia de Batalha'
+def change_name(): 
+    group1_name = 'Criatura'
+
+    group = docRef.LayerSets[group1_name]
+
+    newname = input('Qual o novo nome da carta?')
+    card_name = group.ArtLayers['namea']
+    card_name.TextItem.Contents = newname
+
+change_name()
 
 
 
