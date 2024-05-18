d46289a31ecb739e23294f89256e7275906320a7
Jorge <jorgejapao00@gmail.com>
2024-05-17 22:16:15 -0300

    Now access photoshop

--- cardgenerator.py
+++ cardgenerator.py
@@ -1,4 +1,15 @@
-from comtypes.client import GetActiveObject, CreateObject
+from win32com.client import Dispatch
+
+ps = Dispatch(r"Photoshop.Application")
+file_path = r'C:\Users\jorge\Projetos\Chaos-Card-Generator\PS_templates\ps_template.psd'
 
 
-ps = GetActiveObject("Photoshop.Application")
+docRef = ps.Open(file_path)
+ps.Visible = False
+
+card_name = docRef.ArtLayers['name']
+card_name.TextItem.contents = 'Test Name'
+
+
+
+ps.Quit()
