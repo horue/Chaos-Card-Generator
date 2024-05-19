ba537c72529c2aaa3b08f174ff9270772e2eb4c2
Jorge <jorgejapao00@gmail.com>
2024-05-19 13:23:37 -0300

    Change type function added

--- GUI/cardgenerator.py
+++ GUI/cardgenerator.py
@@ -5,14 +5,18 @@ import os
 ps = Dispatch(r"Photoshop.Application")
 file_path = r'C:\Users\jorge\Projetos\Chaos-Card-Generator\PS_templates\ps_template.psd'
 docRef = ps.Open(file_path)
-output_temp=os.path.join(os.path.expanduser('~'), r'C:\AppData\Local\Temp')
+output_temp = r'C:\Users\jorge\Projetos\Chaos-Card-Generator\temp.png'
 
 
 def save_temp():
-    png_options = Dispatch("Photoshop.PNGSaveOptions")
-    png_options.Interlaced = False
+        if os.path.exists(output_temp):
+            os.remove(output_temp) 
+        # Configurar as opções de salvamento para PNG
+        png_options = Dispatch("Photoshop.PNGSaveOptions")
+        png_options.Interlaced = False
 
-    docRef.SaveAs(output_temp, png_options, True) 
+        # Salvar o documento como PNG no caminho especificado
+        docRef.SaveAs(output_temp, png_options, True) 
 
 
 def change_name(new_name=''): 
@@ -25,6 +29,16 @@ def change_name(new_name=''):
     else:
         card_name = group.ArtLayers['namea']
         card_name.TextItem.Contents = new_name
+
+
+def change_type(new_type=''):
+    group_name = 'Criatura'
+    group = docRef.LayerSets[group_name]
+    if new_type == '':
+        pass
+    else:
+        card_config = group.ArtLayers['type']
+        card_config.TextItem.Contents = new_type
 
 def change_effect(new_effect=''):
     group_name = 'Criatura'
@@ -59,4 +73,6 @@ def change_power():
     else:
         card_config = group.ArtLayers['atk']
         card_config.TextItem.Contents = new_power
+
+
 
